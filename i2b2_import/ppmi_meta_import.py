148db897a860a9af3837ec62347eeba2459a3dff
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-02 15:21:24 +0100

    read PPMI XML metadata

--- i2b2_import/ppmi_meta_import.py
+++ i2b2_import/ppmi_meta_import.py
@@ -1,7 +1,15 @@
-from i2b2_import.meta_import import MetaImport
+import datetime
 import logging
-from defusedxml.ElementTree import parse
+from i2b2_import.meta_import import MetaImport
+from defusedxml import ElementTree
 
+
+DEFAULT_DATE = datetime.datetime(1, 1, 1)
+
+
+########################################################################################################################
+# PUBLIC FUNCTIONS
+########################################################################################################################
 
 class PPMIMetaImport(MetaImport):
     """
@@ -16,6 +24,41 @@ class PPMIMetaImport(MetaImport):
         :param db_conn: Connection to the I2B2 DB.
         :return:
         """
-        root = parse(source).getroot()
-        for project_id in root.findall('projectIdentifier'):
-            logging.info(project_id)
+        tree = ElementTree.parse(source)
+        project_id = tree.find('./project/projectIdentifier')
+        project_descr = tree.find('./project/projectDescription')
+        site = tree.find('./project/siteKey')
+        subject_id = tree.find('./project/subject/subjectIdentifier')
+        subject_sex = tree.find('./project/subject/subjectSex')
+        visit_type = tree.find('./project/subject/visit/visitIdentifier')
+        study_id = tree.find('./project/subject/study/studyIdentifier')
+        subject_age = tree.find('./project/subject/study/subjectAge')
+        age_qualifier = tree.find('./project/subject/study/ageQualifier')
+        subject_age_years = _compute_age_years(subject_age, age_qualifier)
+        post_mortem = tree.find('./project/subject/study/postMortem')
+        series_id = tree.find('./project/subject/study/series/seriesIdentifier')
+        try:
+            acquisition_date = datetime.datetime.strptime(
+                tree.find('./project/subject/study/series/dateAcquired'), '%Y-%m-%d %H:%M:%S.%f')
+        except TypeError:
+            acquisition_date = DEFAULT_DATE
+        image_uid = tree.find('./project/subject/study/series/imagingProtocol/imageUID')
+        image_descr = tree.find('./project/subject/study/series/imagingProtocol/description')
+        protocol = tree.findall('./project/subject/study/series/imagingProtocol/protocolTerm/protocol')
+
+
+########################################################################################################################
+# PRIVATE FUNCTIONS
+########################################################################################################################
+
+def _compute_age_years(subject_age, age_qualifier):
+    if 'Y' == str(age_qualifier).upper():
+        return subject_age
+    elif 'M' == str(age_qualifier).upper():
+        return 0.0833334 * subject_age
+    elif 'W' == str(age_qualifier).upper():
+        return 0.0191781 * subject_age
+    elif 'D' == str(age_qualifier).upper():
+        return 0.00273973 * subject_age
+    else:
+        return None
--- tests/unittest.py
+++ tests/unittest.py
@@ -6,6 +6,7 @@ from nose.tools import assert_equal
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from i2b2_import import db_connection
 from i2b2_import import observation_fact_import
+from i2b2_import import ppmi_meta_import
 
 
 DB_URL = 'postgresql://postgres:postgres@localhost:5432/postgres'
@@ -33,3 +34,6 @@ class TestPublicFunctions:
     def test_01_csv2db(self):
         observation_fact_import.csv2db('./data/features/adni.csv', self.i2b2_db_conn, 'TEST')
         assert_equal(self.i2b2_db_conn.db_session.query(self.i2b2_db_conn.ObservationFact).count(), 12)
+
+    def test_02_xml2db(self):
+        ppmi_meta_import.PPMIMetaImport.meta2i2b2('./data/xml/ppmi.xml', self.i2b2_db_conn)
