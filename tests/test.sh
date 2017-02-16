73c8b58af685a70b7586878f57d408b411242265
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-16 11:36:51 +0100

    update tests to use docker images to deploy db schemas

--- tests/test.sh
+++ tests/test.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-# Start DB container
 echo "Starting DB container..."
-db_docker_id_1=$(docker run -d -p 5433:5432 hbpmip/data-catalog-db:3c4e8d7)
-db_docker_id_2=$(docker run -d -p 5434:5432 hbpmip/i2b2-db:a38fa51)
+db_docker_id_1=$(docker run -d -p 5433:5432 postgres)
+db_docker_id_2=$(docker run -d -p 5434:5432 postgres)
+sleep 3  # TODO: replace this by a test
 
-# Wait for DB to be ready
-echo "Waiting for DB to be ready..."
-sleep 10  # TODO: replace this by a test
+echo "Searching for gateway IP..."
+GATEWAY_IP=$(ip addr | grep docker | grep inet | grep -Eo '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*')
 
-# Run unit tests
+echo "Creating deploying schemas..."
+docker run --rm -e "DB_URL=postgresql://postgres:postgres@$GATEWAY_IP:5433/postgres" hbpmip/data-catalog-setup:1.3.1 upgrade head
+docker run --rm -e "DB_URL=postgresql://postgres:postgres@$GATEWAY_IP:5434/postgres" hbpmip/i2b2-setup:1.3.1 upgrade head
+# c0692da
+sleep 5  # TODO: replace this by a test
+
 echo "Running unit tests..."
 nosetests unittest.py
 ret=$?
@@ -17,8 +21,6 @@ ret=$?
 # Remove DB container (if not on CircleCI)
 if [ -z "$CIRCLECI" ] || [ "$CIRCLECI" = false ] ; then
     echo "Removing DB container..."
-    docker kill ${db_docker_id_1}
-    docker kill ${db_docker_id_2}
     docker rm -f ${db_docker_id_1}
     docker rm -f ${db_docker_id_2}
 fi
