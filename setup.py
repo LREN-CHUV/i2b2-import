516f739468e5a03c1caeba578ce0341d62cad400
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-03-15 17:50:59 +0100

    if no DB is explixitly defined, tries to find config for i2b2 DB

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='i2b2-import',
-    version='1.4.5',
+    version='1.4.6',
     url='https://github.com/LREN-CHUV/i2b2-import',
     description='Import data into an I2B2 DB schema',
     long_description=long_description,
